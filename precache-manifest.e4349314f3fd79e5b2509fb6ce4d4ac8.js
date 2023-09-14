e3c023bbbf5fe368f7afd81607d759ba5d8637b6
amrehab410 <amrehab410@gmail.com>
2023-09-14 22:09:01 +0300

    Updates

---
+++ precache-manifest.e4349314f3fd79e5b2509fb6ce4d4ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d37a9290d67f4431273fc4cd3cfcbc",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "81bced9404db153a006c",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4506de0a536b6dec7bd6",
+    "url": "/my-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "81bced9404db153a006c",
+    "url": "/my-portfolio/static/js/2.dbea407c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-portfolio/static/js/2.dbea407c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4506de0a536b6dec7bd6",
+    "url": "/my-portfolio/static/js/main.90243c47.chunk.js"
+  },
+  {
+    "revision": "28d97e6c4183a7c87d51",
+    "url": "/my-portfolio/static/js/runtime-main.9994cf37.js"
+  }
+]);
